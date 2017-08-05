139096f9f73f4fa174af4020a95897e260d2e40c
Vinnie <vinnie.rossi12@gmail.com>
2017-08-05 14:44:39 -0400

    Add chapter view on blog list
    Blog List will now show each chapter within the given blogs. I have the necessary code to show a small snippet of the blog, but I'll probably have to modify the structure a bit before I do that.
    
    Also added some base css on the blog list page

--- client/src/components/BlogList/BlogList.jsx
+++ client/src/components/BlogList/BlogList.jsx
@@ -1,4 +1,7 @@
 import React, {Component} from 'react';
+import { withRouter } from 'react-router-dom';
+
+import './BlogList.scss';
 
 /*
   The titles are the actual database names for the blogs. This is different from the header, which can be more descriptive.
@@ -7,32 +10,45 @@ import React, {Component} from 'react';
 */
 class BlogList extends Component {
 
-  constructor() {
-    super();
-    this.state = {
-      blogs: [],
-      titles: []
-    };
-  }
+    constructor() {
+      super();
+      this.state = {
+        blogs: [],
+        titles: []
+      };
+      this.navToBlog = this.navToBlog.bind(this);
+    }
 
-  componentDidMount() {
-    fetch('/blogs/')
-      .then(res => res.json())
-      .then(response => {
-        this.setState({
-          blogs: Object.values(response),
-          titles: Object.keys(response)
+    componentDidMount() {
+      fetch('/blogs/')
+        .then(res => res.json())
+        .then(response => {
+          this.setState({
+            blogs: Object.values(response),
+            titles: Object.keys(response)
+          });
         });
-      });
-  }
+    }
+
+    navToBlog = (index, event) => {
+        this.props.history.push(`blogs/${this.state.titles[index]}`);
+    };
+
     render() {
         return (
-        <article>
-            <p> These are all of my blogs: </p>
+        <article className="BlogList">
             {this.state.blogs.map((blog, index) =>
-            <section key={index}>
-              <a href={`#/blogs/${this.state.titles[index]}`}>{blog.header}</a>
-              <br />
+            <section key={index} onClick={(event) => {this.navToBlog(index, event)}}>
+              <div>
+                <a href={`#/blogs/${this.state.titles[index]}`}>{blog.header}</a>
+              </div>
+              
+              {blog.chapters.map((chapter, index) =>
+                <h4 key={index}>{chapter.components[0].props.title}</h4>
+                ,{/* Implement small summary of each chapter
+                <span>{chapter.components[1].props.content[0]}</span>
+              */}
+              )}
             </section>
             )}
         </article>
@@ -40,4 +56,4 @@ class BlogList extends Component {
     }
 }
 
-export default BlogList;
+export default withRouter(BlogList);
